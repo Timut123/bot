c9e67f4ceaf28764239b9ba8fc133b1a7f39b605
Timut123 <tmr56971@gmail.com>
2024-07-08 15:48:41 +0600

    Added HomePage component with styles and imports

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -1,34 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { BiShoppingBag, BiTransfer, BiUser, BiTransferAlt } from 'react-icons/bi'; // Пример иконок, можно выбрать подходящие
-import './HomePage.css'
+import './HomePage.css'; // Импорт CSS файла, где определены стили для HomePage компонента
 
 const HomePage = () => {
   return (
     <div className='HomePage'>
-      <div className='HomePage-IconWrapper'>
-        <BiShoppingBag className='HomePage-Icon' />
-        <Link to='/payments' className='HomePage-Button'>
-          Купить
-        </Link>
-      </div>
-      <div className='HomePage-IconWrapper'>
-        <BiTransfer className='HomePage-Icon' />
-        <Link to='/user' className='HomePage-Button'>
-          Отправить
-        </Link>
-      </div>
-      <div className='HomePage-IconWrapper'>
-        <BiUser className='HomePage-Icon' />
-        <Link to='/market' className='HomePage-Button'>
-          P2P Площадка
-        </Link>
-      </div>
-      <div className='HomePage-IconWrapper'>
-        <BiTransferAlt className='HomePage-Icon' />
-        <Link to='/exchanger' className='HomePage-Button'>
-          Обменик
-        </Link>
+      <div className='HomePage-ButtonRow'>
+        <div className='HomePage-ButtonWrapper'>
+          <Link to='/payments' className='HomePage-Button'>
+            Купить
+          </Link>
+          <BiShoppingBag className='HomePage-Icon' />
+        </div>
+        <div className='HomePage-ButtonWrapper'>
+          <Link to='/user' className='HomePage-Button'>
+            Отправить
+          </Link>
+          <BiTransfer className='HomePage-Icon' />
+        </div>
+        <div className='HomePage-ButtonWrapper'>
+          <Link to='/market' className='HomePage-Button'>
+            P2P Площадка
+          </Link>
+          <BiUser className='HomePage-Icon' />
+        </div>
+        <div className='HomePage-ButtonWrapper'>
+          <Link to='/exchanger' className='HomePage-Button'>
+            Обменик
+          </Link>
+          <BiTransferAlt className='HomePage-Icon' />
+        </div>
       </div>
     </div>
   );
