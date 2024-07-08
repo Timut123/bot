3e6b8d4131b40c54df2011ec987624ad80544625
Timut123 <tmr56971@gmail.com>
2024-07-08 15:36:25 +0600

    Modified HomePage.css Modified HomePage.js

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { BiShoppingBag, BiTransfer, BiUser, BiTransferAlt } from 'react-icons/bi';
+import { BiShoppingBag, BiTransfer, BiUser, BiTransferAlt } from 'react-icons/bi'; // Пример иконок, можно выбрать подходящие
 import './HomePage.css'
 
 const HomePage = () => {
   return (
     <div className='HomePage'>
-      <div className='HomePage-Icons'>
+      <div className='HomePage-IconWrapper'>
         <BiShoppingBag className='HomePage-Icon' />
-        <BiTransfer className='HomePage-Icon' />
-        <BiUser className='HomePage-Icon' />
-        <BiTransferAlt className='HomePage-Icon' />
+        <Link to='/payments' className='HomePage-Button'>
+          Купить
+        </Link>
       </div>
-      <div className='HomePage-Button'>
-        <Link to='/payments'>Купить</Link>
-        <Link to='/user'>Отправить</Link>
-        <Link to='/market'>P2P Площадка</Link>
-        <Link to='/exchanger'>Обменик</Link>
+      <div className='HomePage-IconWrapper'>
+        <BiTransfer className='HomePage-Icon' />
+        <Link to='/user' className='HomePage-Button'>
+          Отправить
+        </Link>
+      </div>
+      <div className='HomePage-IconWrapper'>
+        <BiUser className='HomePage-Icon' />
+        <Link to='/market' className='HomePage-Button'>
+          P2P Площадка
+        </Link>
+      </div>
+      <div className='HomePage-IconWrapper'>
+        <BiTransferAlt className='HomePage-Icon' />
+        <Link to='/exchanger' className='HomePage-Button'>
+          Обменик
+        </Link>
       </div>
     </div>
   );
 }
 
 export default HomePage;
+
